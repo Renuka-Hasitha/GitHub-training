2a9ac9ee130b8ec53c1c47cb70353af1314fabcf
Hasitha <renukahasitha6@gmail.com>
2025-11-05 14:23:34 +0530

    This code can be used to create a gif

---
+++ gif.py
@@ -0,0 +1,8 @@
+import imageio as iio 
+filenames=['/home/hasitha/Documents/team-pic1.png','/home/hasitha/Documents/team-pic2.png']
+images=[]
+for filename in filenames:
+    images.append(iio.imread(filename))
+
+iio.imwrite('team.gif',images,duration=500,loop=0)
+    
