9a656550ff9cff9fae62bd0ef592ffdd3f08e34d
Hasitha <renukahasitha6@gmail.com>
2025-11-07 11:39:45 +0530

    sample

--- gif.py
+++ gif.py
@@ -1,6 +1,11 @@
 import imageio as iio 
 filenames=['/home/hasitha/Documents/team-pic1.png','/home/hasitha/Documents/team-pic2.png']
 images=[]
+
+
+
+
+
 for filename in filenames:
     images.append(iio.imread(filename))
 
